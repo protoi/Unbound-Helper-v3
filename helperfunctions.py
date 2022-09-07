8da7fd5b97f7728d866ab2ba3c555c7d5c7332f3
ImNotTwig <106347250+ImNotTwig@users.noreply.github.com>
2022-09-07 17:44:32 -0500

    Add files via upload

--- helperfunctions.py
+++ helperfunctions.py
@@ -4,19 +4,18 @@ import constants
 # removes all blank spaces, underscores, slash and dash.
 
 
-def normalizeString(text): # removes 
-    return re.sub(constants.normalize_regex, "", text).lower()
+def normalizeString(text):
+    return re.sub("[_\-\\ ]+", "", text).lower()
 
 # overloaded function to format the interpolated string, {}'s -> parameters.
 
 
-# TODO: Replace this with twigs implementation fo StringFormatter
-def StringFormatter(str, a, b, c, d, e, f):
-    return str.format(a, b, c, d, e, f)
+def StringFormatter(str, a, b, c, d=None, e=None, f=None):
+    if d != None:
+        return str.format(a, b, c, d, e, f)
+    else:
+        return str.format(a, b, c)
 
-
-def StringFormatter(str, a, b, c):
-    return str.format(a, b, c)
 
 
 # takes the specified column, normalizes it, then turns the list into a dictionary with key as normalized column, and value as list elements
@@ -24,23 +23,3 @@ def listToDict(columnToUseAsIndex, listToConvert):
     indices = [re.sub(constants.normalize_regex, "", x[columnToUseAsIndex]).lower()
                for x in listToConvert]  # getting the normalized list
     return dict(zip(indices, listToConvert))  # stitching them together
-
-
-def calcScaledStats(bst, hp, at, df, sp, spd, spe):  # replace this with the formula
-    return bst + hp + at + df + sp + spd + spe
-
-
-# returns a formatted string with the pokemon data from stats.json (name, types, stats, generation etc)
-def generateStatScreen(data):
-    return constants.stat_display.format(*(data.values()))
-
-# splits the message into 3 groups, prefix + command(1 word, only alphabets, no spaces/special chars) + (rest of the string)
-
-def msgSplitter(msg, reg):
-    if match := re.search(reg, msg, re.IGNORECASE):
-        title = match.groups()
-        if len(title) <=1:
-            return False 
-        return [ normalizeString(x) for x in title]
-
-    return False
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from dataclasses import field, fields
-from dis import disco
-from tabnanny import check
+from locale import normalize
 import discord
 import os
 import numpy as np
@@ -9,17 +7,13 @@ import json
 import helperfunctions
 import constants
 from dotenv import load_dotenv
-import re
 
 load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-# other than the tm list.json and stats.json(if we want to index it with their ID too), everything is probably easier to query
-
-splitter = constants.message_checker_regex.format(constants.prefix)
-
+# other than the tm list, everything is probably easier to query
 
 #################################### GENERATING DICTIONARIES ###########################################
 with open("DATA/abilities.json", encoding='utf8') as file:
@@ -44,8 +38,8 @@ with open("DATA/tm_and_tutor.json", encoding='utf8') as file:
     tm_and_tutor_dict = helperfunctions.listToDict('name',  json.load(file))
 with open("DATA/zlocation.json", encoding='utf8') as file:
     zlocation_dict = helperfunctions.listToDict('name',  json.load(file))
-with open("DATA/stats.json", encoding='utf8') as file:
-    stats_dict = helperfunctions.listToDict('name',  json.load(file))
+with open("DATA/PokemonStatsNormal.json", encoding='utf8') as file:
+    normal_stats_dict = helperfunctions.listToDict('name',  json.load(file))
 ######################################################################################################
 
 
@@ -59,57 +53,32 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith(constants.prefix):
-        embedToSend = None
-        query = message.content
-        inputs = helperfunctions.msgSplitter(query, splitter)
-        if inputs == False:  # not a message we are interested in
-            return
-            
-        if(inputs[1] == 'pokedata'):                                        #POKEDATA
-            if(len(inputs) < 3):                                            # check if there is a key associated with the command
-                await message.channel.send(constants.invalid_text)          #error message
-                return
-            stat_element = stats_dict.get(inputs[2], False)                 # query dictionary 
-            if stat_element == False:                                       # is key not present, display error message and break out of it
-                await message.channel.send(constants.invalid_text)
-                return
-            embedBody = helperfunctions.generateStatScreen(stat_element)    #obtain formatted string
-            embedToSend = discord.Embed(
-                title=stat_element.get('name',
-                'place_holder_name'),
-                description=embedBody)                                      #create embed
-            await message.channel.send(embed = embedToSend)                 #post embed
-        
-        elif(inputs[1] == 'help'):                                          #HELP
-            embedToSend = discord.Embed(title= 'Help')                      #sets title as help, don't care about rest of the message
-            for index, (n,v) in enumerate(constants.command_text):          #looping over the commant_text list for name and value pairs
-                embedToSend.add_field(                                      #adding the fields one at a time
-                    name=constants.prefix + n,
-                    value=v, 
-                    inline=False)                                           #inline commands not supported on mobile, it lets you have atmost 3 columns in your embeds
-            await message.channel.send(embed = embedToSend)                 #sending the embed
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
-client.run(os.getenv('tok'))
+# client.run(os.getenv('tok'))
+
+# sets hp to 1, attack to 2, etc 
+#s = constants.stat_display.format(1, 2, 3, 4, 5, 6)
+s = helperfunctions.StringFormatter(constants.stat_display, 1,2,3,4,5,6)
+
+#prints the stats formatted
+print(s)
+
+print(tmlocation_dict[helperfunctions.normalizeString('focus                       punch')]) #<- returns a dictionary
 
 
-######################################CODE FOR TESTING###################################
-'''x = stats_dict.get(helperfunctions.normalizeString('king ler'), False) #use this to query
-print(x)
-print(helperfunctions.generateStatScreen(x))
 
+#normalizes string to galarian darmanitan
+dic = abilities_dict[helperfunctions.normalizeString('GALARIAN darManiTAN')]
+#sets name to the "name" field from galarian darmanitan in the query
+name = dic['name']
+#sets the ability dict to the "abilities" field in the galarian darmanitan entry 
+ability = dic['Ability']
 
-s1=";scale pokemon name"
-s2=";   tmlocation focus punch"
-s3="  ;     help"
-s4="   ;zmove Kommomium-Z"
-s5 = "; help help help help"
-
-myreg = constants.message_checker_regex.format(constants.prefix)
-
-print(helperfunctions.msgSplitter(s1, myreg))
-print(helperfunctions.msgSplitter(s2, myreg))
-print(helperfunctions.msgSplitter(s3, myreg))
-print(helperfunctions.msgSplitter(s4, myreg))
-'''
-#########################################################################################
+ability_info = helperfunctions.StringFormatter(constants.ability_display, ability[0], ability[1], ability[2])
+#prints "ability for Galarian Darmanitan: 
+#        Ability 1:Gorilla Tactics
+#        Ability 2:None
+#        Hidden Ability:Zen Mode"
+print(f'ability for {name}:\n{ability_info}')
