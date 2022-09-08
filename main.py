de134abfea9e8208067146d18b1c2ee21573d8ec
protoi <protikdatta9@gmail.com>
2022-09-09 01:27:15 +0530

    tm command working, created a mapping of tm number with tm name

--- main.py
+++ main.py
@@ -23,26 +23,38 @@ def joinstr(a):
 #################################### GENERATING DICTIONARIES ###########################################
 with open("DATA/abilities.json", encoding='utf8') as file:
     abilities_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/ability_description.json", encoding='utf8') as file:
     ability_desc_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/eggmoves.json", encoding='utf8') as file:
     eggmoves_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/helditem.json", encoding='utf8') as file:
     helditem_dict = helperfunctions.listToDict('itemname',  json.load(file))
+
 with open("DATA/lvlupmoves.json", encoding='utf8') as file:
     lvlupmoves_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/megastone.json", encoding='utf8') as file:
     megastone_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/pokelocation.json", encoding='utf8') as file:
     pokelocation_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/scalemon.json", encoding='utf8') as file:
     scalemon_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/tmlocation.json", encoding='utf8') as file:
     tmlocation_dict = helperfunctions.listToDict('tmname',  json.load(file))
+    tm_name_number_mapping = dict(zip( np.char.mod('%d', np.arange(1, 121, 1)) , tmlocation_dict.keys())) #making a number string mapping for the tm dictionary
+
 with open("DATA/tm_and_tutor.json", encoding='utf8') as file:
     tm_and_tutor_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/zlocation.json", encoding='utf8') as file:
     zlocation_dict = helperfunctions.listToDict('name',  json.load(file))
+
 with open("DATA/stats.json", encoding='utf8') as file:
     stats_dict = helperfunctions.listToDict('name',  json.load(file))
 ######################################################################################################
@@ -170,13 +182,37 @@ async def on_message(message):
                 description=embedBody)                                      
             await message.channel.send(embed=embedToSend)                   #sending the embed
 #___________________________________________________________________________________________________________                    
+        elif(inputs[1] == 'tm'):                                            #TM
+            if(len(inputs) < 3):                                            #checking whether message has anything after the command
+                await message.channel.send(constants.invalid_text)          #invalid message
+                return 
+            
+            q = inputs[2]
 
+            searchThis = tm_name_number_mapping.get(q, q)                   #checking if the query is present in the mapping
+                                                                            #can only be possible if query was a number between 1 to 120 (inclusive)
+                                                                            # obtain name of tm to search here 
+            
+            tmlocation_element = tmlocation_dict.get(searchThis, False)     #querying using the name now
+            
+            if tmlocation_element == False:                                 #if no dicitonary found, jump out of this
+                await message.channel.send(constants.invalid_text)          #error message
+                return
+            embedTitle = "TM# "+str(tmlocation_element['tmnumber'])         #extracting the name of the pokemon
+            embedBody = f'''{tmlocation_element['tmname'].title()}
+            {tmlocation_element['tmlocation']}'''                           #tm name + tm location in body
+            embedToSend = discord.Embed(
+                title=embedTitle,
+                description=embedBody)                                      #producing an embed
+            await message.channel.send(embed=embedToSend)                   #sending the embed
+#___________________________________________________________________________________________________________       
+     
 
 client.run(os.getenv('tok'))
 
 
 ######################################CODE FOR TESTING###################################
-x = stats_dict.get(helperfunctions.normalizeString('galarian Darmanitan'), False) #use this to query
+# x = stats_dict.get(helperfunctions.normalizeString('galarian Darmanitan'), False) #use this to query
 #print(x)
 #print(helperfunctions.generateStatScreen(x))
 
